863b69210bfd0a399a878313c62090549d7b9d31
mutiarahns <mutiarahns@gmail.com>
2025-02-26 20:52:55 +0700

    chore: tidy up

---
+++ src/data/vegetable.ts
@@ -0,0 +1,46 @@
+import { Vegetable } from "../types/vegetables";
+
+export const vegetables: Vegetable[] = [
+  {
+    id: 1,
+    name: "Carrot",
+    classification: "Roots",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+  {
+    id: 2,
+    name: "Tomato",
+    classification: "Fruits",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+  {
+    id: 3,
+    name: "Lettuce",
+    classification: "Leaves",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+  {
+    id: 4,
+    name: "Pumkin",
+    classification: "Fruits",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+  {
+    id: 5,
+    name: "Okra",
+    classification: "Fruits",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+  {
+    id: 6,
+    name: "Cucumber",
+    classification: "Fruits",
+    createdAt: new Date().toISOString(),
+    updatedAt: null,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,10 @@
 import { Hono } from "hono";
 import { Vegetable } from "./types/vegetables";
+import { vegetables as vegetablesData } from "./data/vegetable";
 
 const app = new Hono();
 
-let vegetables: Vegetable[] = [
-  {
-    id: 1,
-    name: "Carrot",
-    classification: "Roots",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-  {
-    id: 2,
-    name: "Tomato",
-    classification: "Fruits",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-  {
-    id: 3,
-    name: "Lettuce",
-    classification: "Leaves",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-  {
-    id: 4,
-    name: "Pumkin",
-    classification: "Fruits",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-  {
-    id: 5,
-    name: "Okra",
-    classification: "Fruits",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-  {
-    id: 6,
-    name: "Cucumber",
-    classification: "Fruits",
-    createdAt: new Date().toISOString(),
-    updatedAt: null,
-  },
-];
+let vegetables = vegetablesData;
 
 app.get("/", (c) => {
   return c.json({
